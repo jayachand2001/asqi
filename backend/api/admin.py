71fca1184e547f06c49b8a9802a51f108ede6622
jayachand2001 <jayachand2001@gmail.com>
2023-11-27 08:50:33 +0530

    first commit

---
+++ backend/api/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from api.models import User, Profile, Todo
+
+# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email']
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_editable = ['verified']
+    list_display = ['user', 'full_name' ,'verified']
+
+class TodoAdmin(admin.ModelAdmin):
+    list_editable = ['completed']
+    list_display = ['user', 'title' ,'description','completed', 'created']    
+
+admin.site.register(User, UserAdmin)
+admin.site.register( Profile,ProfileAdmin)  
+admin.site.register( Todo,TodoAdmin)    
---
+++ backend/api/urls.py
@@ -0,0 +1,16 @@
+from rest_framework_simplejwt.views import TokenRefreshView
+from django.urls import path
+from api import views
+
+urlpatterns = [
+    path("token/", views.MyTokenObtainPairView.as_view()),
+    path("token/refresh/", TokenRefreshView.as_view()),
+    path("register/", views.RegisterView.as_view()),
+    path('test/', views.testEndPoint, name='test'),
+    path('', views.getRoutes),
+
+    #Todo urls
+    path("todo/<user_id>/", views.TodoListView.as_view()),
+    path("todo-detail/<user_id>/<todo_id>/", views.TodoDetailView.as_view()),
+    path("todo-mark-as-completed/<user_id>/<todo_id>/", views.TodoMarkAsCompleted.as_view()),
+]
